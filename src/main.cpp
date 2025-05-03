91d470393edc3b7d403cdc4987a7fe8d1561fb79
IsaiahDolbier <73249502+IsaiahDolbier@users.noreply.github.com>
2025-05-02 18:20:29 -0600

    servo adjustments

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,13 @@ const unsigned long logInterval = 20;
 
 int GreenLedPin = 13;
 
+unsigned long initTimeTaken;
 
 //Calls initialization routines(initSensors, initServos, ) during setup()
 //Repeatedly updates sensors and control surfaces in loop()
 void setup() {
   Serial.begin(115200);
-
+  delay(60000);
   initSensors();
   initServos();
   initSDCard();
@@ -24,14 +25,16 @@ void setup() {
     updateAltitude();
     updateIMUandServos();
   }
+
   digitalWrite(GreenLedPin, HIGH);
+  initTimeTaken = millis();
 }
 
 void loop() {
   updateAltitude();
   updateIMUandServos();
 
-  unsigned long now = millis();
+  unsigned long now = millis() - initTimeTaken;
   if (now - lastLogTime >= logInterval)
   {
     SDCardWrite(now);
